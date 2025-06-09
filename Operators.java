e2f0f415105c1ecc07fd81fcf74b053726539563
shraddhay0821 <shraddhay423@gmail.com>
2025-06-09 02:12:42 -0700

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,15 @@
+package myfirstpackage;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int Maths = 30;
+		int Science = 40;
+		int English = 30;
+	    int avg = ((Maths+Science+English)/3);
+				System.out.println("Total Marks"+ (Maths+Science+English));
+				System.out.println("Average marks" +avg);
+
+	}
+
+}
