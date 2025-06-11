7a4dceb1f1bfc0af5fbca32ea8ebe1703954ec13
shraddhay0821 <shraddhay423@gmail.com>
2025-06-11 07:28:25 -0700

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,14 @@
+package myfirstpackage;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		int number=5;
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println(number + " * " +  i+ " = " + (number*i));
+			
+		}
+	}
+
+}
