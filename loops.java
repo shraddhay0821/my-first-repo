dab86249ba2a13135164897375f06ed16a56d815
shraddhay0821 <shraddhay423@gmail.com>
2025-06-11 07:31:15 -0700

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,17 @@
+package myfirstpackage;
+
+public class loops {
+
+	public static void main(String[] args) {
+		int number=4567;
+		int count=0;
+		while (number!=0) {
+			number=number/10;
+			count++;
+				}
+		System.out.print("total number : " + count);
+			
+
+	}
+
+}
