478a2ebb70a3c5492c71fa44a768aa720a4e8d7e
shraddhay0821 <shraddhay423@gmail.com>
2025-06-11 23:07:12 -0700

    Add files via upload

---
+++ Tables.java
@@ -0,0 +1,17 @@
+package myfirstpackage;
+
+public class Tables {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+	           
+	           for (int j = 1; j <= 5; j++) {
+	                System.out.print(j + " x " + i + " = " + (j * i)+ "\t" );
+	            }
+	            System.out.println(); 
+	        }
+
+
+	}
+
+}
